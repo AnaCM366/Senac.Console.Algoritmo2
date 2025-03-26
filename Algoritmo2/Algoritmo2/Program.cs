2493b39d05921cd07b85276516435470a32397a3
Ana Carolina Machado <anacarolinamachado366@gmail.com>
2025-03-25 21:50:19 -0300

    LEITURA DE VARIAVEIS

--- Algoritmo2/Algoritmo2/Program.cs
+++ Algoritmo2/Algoritmo2/Program.cs
@@ -5,3 +5,16 @@ char letra = 'a';
 var nomeCompleto = "Ana Carolina Machado";
 var altura = 167;
 
+Console.WriteLine("Digite seu nome");
+nomeCompleto  = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Bem vindo " + nomeCompleto);
+Console.WriteLine();
+Console.Write("Digite sua idade: ");
+var linha = Console.ReadLine();
+idade = int.Parse(linha);
+Console.WriteLine("Idade informada " + idade);
+if (idade < 18)
+    Console.WriteLine("Você não pode estar aqui!");
+
+Console.ReadKey();
